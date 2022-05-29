0138db069d6ed072165772ef23031c22c25dc7f7
Prince Thind <princethind799@gmail.com>
2022-05-29 21:46:17 +0530

    add bombs

--- src/main.js
+++ src/main.js
@@ -86,10 +86,27 @@ function create() {
   this.physics.add.overlap(player, stars, collectStar, null, this);
 
   window.score = 0;
-  window.scoreText=this.add.text(16, 16, "score: 0", {
+  window.scoreText = this.add.text(16, 16, "score: 0", {
     fontSize: "32px",
     fill: "#000",
   });
+
+  window.bombs = this.physics.add.group();
+
+  this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(bombs, bombs);
+
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+  function hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play("turn");
+
+    window.gameOver = true;
+  }
 }
 
 function update() {
@@ -112,8 +129,27 @@ function update() {
   }
 }
 
-function collectStar(player, star) {
-  star.disableBody(true, true);
-  score += 10;
-  scoreText.setText("Score: " + score);
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(0.1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
 }
